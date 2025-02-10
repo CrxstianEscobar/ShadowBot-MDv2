1764c15874bf5deac02115d62119c2c234ecf18b
Cristian Escobar <cristianyhacson2008@gmail.com>
2025-02-10 15:15:45 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -7,21 +7,16 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 import moment from 'moment-timezone' 
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
-global.botNumberCode = '' //Ejemplo: +573218138672
+global.botNumberCode = '' //Ejemplo: +51965911060
 global.confirmCode = ''
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 global.owner = [
-   ['59168683798', 'Creador 👑', true],
-   ['51968382008', 'Owner 🍭', true],
-   ['59897246324', 'Owner 🍧', true],
-   ['573012482597'],
-   ['5351524614'],
-   ['5218711426787']
+   ['51927238856', 'Creador', true],
+   ['51965911060']
 ]
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
@@ -37,29 +32,28 @@ global.libreria = 'Baileys'
 global.baileys = 'V 6.7.5'
 global.vs = '2.0.7'
 global.vsJB = '5.0'
-global.nameqr = 'Genesis-Ultra'
-global.namebot = 'gᥱᥒᥱsіs ᥙᥣ𝗍rᥲ ᰔᩚ'
-global.sessions = 'GenesisSession'
-global.jadi = 'GenesisJadiBot'
+global.nameqr = 'ShadowBot-MD'
+global.namebot = 'Shadow Bot ᰔᩚ'
+global.sessions = 'ShadowSession'
+global.jadi = 'ShadowJadiBot'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.packname = '⪛✰ Genesis Ai - Ultra ✰⪜'
-global.botname = 'gᥱᥒᥱsіs-ᥲі'
-global.wm = 'Genesis-Ultra 🤍'
-global.author = 'mᥲძᥱ ᑲᥡ : іzᥙmі.kz᥊ ☕'
-global.dev = '© ⍴᥆ᥕᥱrᥱძ ᑲᥡ ᥴ᥆ძᥱ 𝗍і𝗍ᥲᥒs'
-global.textbot = 'Genesis Ai : Izumi.kzx 🤍'
-global.namebot = 'Genesis Ai'
-global.stickpack = `© ⍴᥆ᥕᥱrᥱძ ᑲᥡ ᥴ᥆ძᥱ 𝗍і𝗍ᥲᥒs`
-global.titulowm = '-❀ᩙ̈͟༚̮ ⡞᪲=͟͟͞🄶𝚎᪶۫۫𝚗᪶۫۫𝚎᪶۫۫𝚜᪶۫۫𝚒᪶۫۫𝚜᪶۫ 𝚊᪶۫𝚒᪶۫͜ ≼᳞ׄ ᵎ ˚꙳꤬ꨪ'
-global.titulowm2 = '.‧·ீ੭ ¡ ᗃᮢ፝֟͡Gᴇɴᴇsɪs B۵ᴛ ʟᴀ ᴍᴇᴊ꧔ʀ !˚̩̩̥͙°̩̥༅˚'
-global.igfg = '@angelito.kzx'
-global.titu = '©️ ρσωε૨ ɓყ ɠαℓαאყ ƭεαɱ'
+global.packname = '⪛✰ Shadow ✰⪜'
+global.botname = 'Shadow MD'
+global.wm = 'Shadow 🤍'
+global.author = 'mᥲძᥱ ᑲᥡ : Criss7 ☕'
+global.dev = '© ⍴᥆ᥕᥱrᥱძ ᑲᥡ Heavenly'
+global.textbot = 'Genesis Ai : Criss7 🤍'
+global.namebot = 'Shadow Bot - MD'
+global.stickpack = `© ⍴᥆ᥕᥱrᥱძ ᑲᥡ ᥴ᥆ძᥱ Heavenly`
+global.titulowm = '-❀ᩙ̈͟༚̮ ⡞᪲=͟͟͞Shx ≼᳞ׄ ᵎ ˚꙳꤬ꨪ'
+global.titulowm2 = '.‧·ீ੭ ¡ ᗃᮢ፝֟͡Shxx B۵ᴛ ᴍᴇᴊ꧔ʀx !˚̩̩̥͙°̩̥༅˚'
+global.titu = '©️ ρσωε૨ ɓყ Shx'
 global.listo = '*Aqui tiene ฅ^•ﻌ•^ฅ*'
-global.vs = '2.0.7'
-global.namechannel = '𝑮𝒆𝒏𝒆𝒔𝒊𝒔-𝑩𝒐𝒕 - 𝑪𝒉𝒂𝒏𝒏𝒆𝒍'
-global.stickauth = `© Genesis Ai By Izumi.kzx`
+global.vs = '2.0.0'
+global.namechannel = 'Shadow Ultra'
+global.stickauth = `© Ultra Ai By Criss`
 global.dis = ':⁖֟⊱┈֟፝❥'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
@@ -67,16 +61,16 @@ global.dis = ':⁖֟⊱┈֟፝❥'
 global.catalogo = fs.readFileSync('./src/catalogo.png')
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
+/*
 global.group = 'https://chat.whatsapp.com/GqKwwoV2JJaJDP2SL7SddX'
 global.group2 = 'https://chat.whatsapp.com/Fn5Ipyxu6mE6qEQlwWZTwU'
 global.canal = 'https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y'
 global.github = 'https://github.com/Angelito-OFC/Genesis-AI' 
 global.instagram = 'https://www.instagram.com/angelito.kzx' 
 global.whatsApp = 'https://wa.me/59168683798'
-
+*/
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
+/*
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: 'ᰔᩚ gᥱᥒᥱsіs ᥕһᥲ𝗍sᥲ⍴⍴ ᑲ᥆𝗍', orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}};
 
 global.fakegif2 = { key: { participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { videoMessage: { title: 'GenesisBot-MD', h: `Hmm`, seconds: '99999', gifPlayback: true, caption: '⚘݄𖠵⃕⁖𖥔.𝐁𝐢𝐞𝐧𝐯𝐞𝐧𝐢𝐝𝐨❞ ꔷ──᜔◇⃟̣̣⃕✨', jpegThumbnail: catalogo }}};
@@ -92,7 +86,7 @@ ch1: '120363220939514640@newsletter',
 ch2: '120363343366246815@newsletter',
 ch3: '120363371366801178@newsletter'
 }
-
+*/
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 global.APIs = { // API Prefix
